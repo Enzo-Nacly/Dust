28bbe91a7ba2e9a6aad3437c6f6af779533c4c0b
Enzo-Nacly <enzoabenassiff@gmail.com>
2026-05-11 20:21:03 -0300

    renomeia scripts para PascalCase

---
+++ scripts/csharp_scripts/Estrutura.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Estrutura : StaticBody2D
+{
+	private Planet planet;
+	private RotationComponent rotationComponent;
+	public override void _Ready()
+	{
+		rotationComponent = GetNode<RotationComponent>("Rotation_Component");
+		rotationComponent.Setup(this);
+
+		planet = GetTree().GetNodesInGroup("planets")[0] as Planet;
+	}
+
+	public override void _Process(double delta)
+	{
+		Vector2 forcaGravitacional = planet.PegarGravidadeEm(GlobalPosition);
+
+        if (forcaGravitacional != Vector2.Zero)
+        {
+            Vector2 vetorCentro = forcaGravitacional.Normalized();
+            rotationComponent.AtualizarRotacao(vetorCentro);
+		}
+	}
+}
--- scripts/gdscripts/player.gd
+++ scripts/gdscripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @onready var movement_component : MovementComponent = $Components/Movement_Componet
 @onready var rotation_component : Rotation_Component = $Components/Rotation_Component
 @onready var jump_component : Jump_Component = $Components/Jump_Component
-@onready var planet : Planet = get_tree().get_nodes_in_group("planets")[0]
+#@onready var planet : Planet = get_tree().get_nodes_in_group("planets")[0]
 
 func _ready() -> void:
 	movement_component.setup(self)
@@ -15,24 +15,24 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	movement_component.mover(delta)
 	
-	var forca_gravitacional : Vector2 = planet.pegar_gravidade_em(self.global_position)
+	#var forca_gravitacional : Vector2 = planet.pegar_gravidade_em(self.global_position)
 	
 	# se o explorer não está fora de um corpo celeste
-	if forca_gravitacional != Vector2.ZERO:
-		var vetor_centro : Vector2 = forca_gravitacional.normalized()
-		rotation_component.atualizar_rotacao(vetor_centro)
+	#if forca_gravitacional != Vector2.ZERO:
+		#var vetor_centro : Vector2 = forca_gravitacional.normalized()
+		#rotation_component.atualizar_rotacao(vetor_centro)
 		
 		# settando corretamente em relação ao planeta
-		self.up_direction = -vetor_centro
+		#self.up_direction = -vetor_centro
 		
 		# atualizando o motion_mode para não quebrar o .is_on_floor() no jump_component
-		self.motion_mode = CharacterBody2D.MOTION_MODE_GROUNDED
-		jump_component.pular(vetor_centro)
+		#self.motion_mode = CharacterBody2D.MOTION_MODE_GROUNDED
+		#jump_component.pular(vetor_centro)
 		
 		# aplicando gravidade a entidade
-		self.velocity += forca_gravitacional * delta
-	else:
+		#self.velocity += forca_gravitacional * delta
+	#else:
 		# atualizando o motion_mode para não quebrar o .is_on_floor() no jump_component
-		self.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+		#self.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 	
 	move_and_slide()
