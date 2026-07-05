31e4b00eb1602c625635f55a95de2c4f0dd54d4e
Enzo-Nacly <enzoabenassiff@gmail.com>
2026-07-05 15:16:54 -0300

    animcao tableta corrigida

--- scripts/tableta/tableta.gd
+++ scripts/tableta/tableta.gd
@@ -1,44 +1,84 @@
 extends Node2D
-@onready var interaction_area : InteractionArea = $InteractionArea
+
+@onready var interaction_area: InteractionArea = $InteractionArea
 @onready var tableta_animation_player: AnimationPlayer = $tableta_animation_player
 @onready var tela_animation_player: AnimationPlayer = $tela_animation_player
 @onready var tela_sprite: Sprite2D = $InteractionArea/CanvasLayer/tableta_tela_sprite
 
+var player_perto: bool = false
+
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "InteracaoTablet1")
-	interaction_area.body_entered.connect(Callable(self, "_on_body_entered"))
-	interaction_area.body_exited.connect(Callable(self, "_on_body_exited"))
-	
-	
-func _on_body_entered(body: Node2D) -> void:	
-	if body.name == "Player":
-		print("player entrou \nligando\n")	
-		
-		tableta_animation_player.play("ligando")
-		
-		#if tableta_animation_player.current_animation != "desligando":
-			#print("aguardando animacao acabar on_entered\n")
-			#await tableta_animation_player.animation_finished
-			#print("inicializando tela\n")
-			#tableta_animation_player.play("inicializar_tela")
-			#await tableta_animation_player.animation_finished
-			#tableta_animation_player.play("ligada")
+
+	interaction_area.body_entered.connect(_on_body_entered)
+	interaction_area.body_exited.connect(_on_body_exited)
+
+	tableta_animation_player.animation_finished.connect(_animacao_acabou)
+
+
+func _animacao_acabou(animacao_nome: String) -> void:
+	match animacao_nome:
+		"ligando":
+			if player_perto:
+				tableta_animation_player.play("inicializar_tela")
+			else:
+				tableta_animation_player.play("desligando")
+		"inicializar_tela":
+			if player_perto:
+				tableta_animation_player.play("ligada")
+			else:
+				tableta_animation_player.play("desligando")
+		"desligando":
+			pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name != "Player":
+		return
+
+	player_perto = true
+
+	print("Player entrou")
+
+	# Evita reiniciar a sequência caso ela já esteja ligada
+	if tableta_animation_player.current_animation in [
+		"ligando",
+		"inicializar_tela",
+		"ligada"
+	]:
+		return
+
+	tableta_animation_player.play("ligando")
 
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.name == "Player":
-		print("player saiu \ndesligando\n")
-		
-		tela_sprite.visible = false
+	if body.name != "Player":
+		return
+	
+	player_perto = false
+	print("Player saiu")
+	
+	# Esconde a tela imediatamente
+	tela_sprite.visible = false
+	
+	if tableta_animation_player.current_animation != "desligando":
 		tableta_animation_player.play("desligando")
-		
 
-func InteracaoTablet1():
-	if tela_sprite.visible == false:
+
+func InteracaoTablet1() -> void:
+	if !player_perto:
+		return
+
+	if tableta_animation_player.current_animation != "ligada":
+		return
+
+	if !tela_sprite.visible:
 		tela_sprite.visible = true
 		tela_animation_player.play("on_off")
+
 		await tela_animation_player.animation_finished
+
 		tela_animation_player.play("ligada")
 	else:
 		tela_sprite.visible = false
